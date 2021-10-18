b3edc13d6d0be44fe7d6fa3f7531e6f8317af1ad
barerenate <renatw17@uia.no>
2021-10-18 13:46:54 +0200

    create basis

---
+++ script.js
@@ -0,0 +1,6 @@
+function openForm() {
+    document.getElementById("checkpop").style.display = "block";
+}
+function closeForm() {
+    document.getElementById("checkpop").style.display = "none";
+}
